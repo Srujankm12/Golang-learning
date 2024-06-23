7074f66b41989ff585357ad5bd9dbd918e9a3daf
Srujankm12 <srujankm12@gmail.com>
2024-06-23 18:52:42 +0530

    newlearning

--- praser/main.go
+++ praser/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	//"log"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"praser/handlers"
+	"time"
 )
 
 func main() {
@@ -19,9 +22,35 @@ func main() {
 	// })
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gb := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gb)
 
-	http.ListenAndServe(":8001", sm)
+	s := http.Server{
+		Addr:         ":8001",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	//http.ListenAndServe(":8001", sm)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+
+	}()
+	sigcha := make(chan os.Signal)
+	signal.Notify(sigcha, os.Interrupt)
+	signal.Notify(sigcha, os.Kill)
+
+	sig := <-sigcha
+	l.Println("Recived terminate ,gracefull shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
