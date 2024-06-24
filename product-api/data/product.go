30efa33740ef1d2f6faa6c638aaae136e558fbf7
Srujankm12 <srujankm12@gmail.com>
2024-06-24 23:45:06 +0530

    error day

--- product-api/data/product.go
+++ product-api/data/product.go
@@ -1,1 +1,35 @@
 package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+var ProductList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "best coffee",
+		Price:       90.0,
+		SKU:         "abc123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "strong coffee",
+		Price:       85.0,
+		SKU:         "def456",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
--- product-api/main.go
+++ product-api/main.go
@@ -1,50 +1,17 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
-	"newprac/handlers"
 	"os"
-	"os/signal"
-	"time"
 )
 
 func main() {
 
 	l := log.New(os.Stdout, "prouct-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gb := handlers.NewGoodbye(l)
-
+	ph := handlers.NewProduct(l)
 	sm := http.NewServeMux()
-	sm.Handle("/goodbye", gb)
-	sm.Handle("/", hh)
 
-	s := &http.Server{
-
-		Addr:         ":8010",
-		Handler:      sm,
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
-	}
-	go func() {
-
-		err := s.ListenAndServe()
-
-		if err != nil {
-			l.Fatal(err)
-		}
-	}()
-
-	sigcha := make(chan os.Signal)
-	signal.Notify(sigcha, os.Interrupt)
-	signal.Notify(sigcha, os.Kill)
-
-	sig := <-sigcha
-	l.Println("recived terminate ,graceful shutdown", sig)
-
-	tc, _ := context.WithTimeout(context.Background(), 120*time.Second)
-	s.Shutdown(tc)
+	sm.Handle("/", ph)
 
 }
